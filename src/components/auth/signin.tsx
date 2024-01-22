03864ff7e08a4c2ca2bd73472573edb2fe8a2b0d
Kleuber Silva <kleuber20@hotmail.com>
2024-01-22 13:55:40 -0300

    Design in page signin

--- src/components/auth/signin.tsx
+++ src/components/auth/signin.tsx
@@ -6,10 +6,14 @@ import * as z from 'zod';
 import useSupabase from '@/hooks/useSupabase';
 import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import { Button } from '../ui/button';
+import Link from 'next/link';
 
 const schema = z.object({
-    email: z.string().email(),
-    password: z.string(),
+    email: z.string().email({ message: "Email inválido." }),
+    password: z.string().min(8, { message: "A senha deverá conter no mínimo 8 caracteres." }),
 });
 
 type UserFormData = z.infer<typeof schema>
@@ -29,7 +33,6 @@ export default function SignInComponent() {
 
     async function isAuthenticated() {
         const { data } = await supabase.auth.getSession();
-        console.log(data);
         if (data.session?.user) {
             router.push('/home')
         }
@@ -49,9 +52,9 @@ export default function SignInComponent() {
                 {
                     style: {
                         borderRadius: '5px',
-                        background: '#333',
+                        background: '#1c1c1c',
                         color: '#fff',
-                        border: '1px solid #3d3d3d'
+                        border: '1px solid #2e2e2e'
                     },
                 });
         } else {
@@ -59,23 +62,66 @@ export default function SignInComponent() {
                 {
                     style: {
                         borderRadius: '5px',
-                        background: '#333',
+                        background: '#1c1c1c',
                         color: '#fff',
-                        border: '1px solid #3d3d3d'
+                        border: '1px solid #2e2e2e'
                     },
                 });
         }
     }
 
     return (
-        <section>
-            <form onSubmit={handleSubmit(submitHandler)}>
-                <input {...register('email')} type='email' />
-                {errors.email?.message && <p>{errors.email?.message}</p>}
-                <input type="password" {...register('password')} />
-                {errors.password?.message && <p>{errors.password?.message}</p>}
-                <input type="submit" />
+        <section className='w-full h-full flex flex-col justify-center items-center'>
+            <div className='flex items-start w-[25dvw] flex-col gap-2'>
+                <div></div>
+                <h1 className='text-2xl text-white font-bold font-mono'>Bem vindo de volta</h1>
+                <span className='text-sm text-[#6f6f6f] font-mono font-medium'>Faça login em sua conta</span>
+            </div>
+            <form onSubmit={handleSubmit(submitHandler)} className='flex flex-col gap-10 w-[25dvw] h-[50dvh] justify-center'>
+
+                <div>
+                    <div className='pb-2'>
+                        <Label htmlFor='email' className='text-[#6e6e6e]'>Email</Label>
+                    </div>
+                    <Input
+                        {...register('email')}
+                        type='email'
+                        placeholder='youemail@example.com'
+                        id='email'
+                        className={`${errors.email?.message && 'border-[#d25151] bg-[#d25151] bg-opacity-20 transition-all duration-200'}`} />
+                    {errors.email?.message &&
+                        <span
+                            className='text-xs text-[#d25151]'
+                        >
+                            {errors.email?.message}
+                        </span>
+                    }
+                </div>
+
+                <div>
+                    <div className='flex justify-between pb-2'>
+                        <Label htmlFor='password' className='text-[#6e6e6e]'>Password</Label>
+                        <Link href="/forgot-password" className='text-[#6e6e6e] text-sm font-semibold font-mono'>Esqueceu a Senha?</Link>
+                    </div>
+                    <Input
+                        type="password"
+                        id='password'
+                        {...register('password')}
+                        placeholder='********'
+                        className={`${errors.password?.message && 'border-[#d25151] bg-[#d25151] bg-opacity-20 transition-all duration-200'}`}
+                    />
+                    {errors.password?.message && <span className='text-xs text-[#d25151]'>{errors.password?.message}</span>}
+                </div>
+                <Button type="submit" variant="signIn" >Sign In</Button>
+                <div>
+                    <span className='text-[#6e6e6e] text-sm font-semibold font-mono'>Não tem uma conta? <Link href="/auth/signup" className='text-white hover:underline transition-all duration-150'>Inscreva-se agora</Link></span>
+                </div>
             </form>
+            <div className='w-[25dvw] text-[#6e6e6e] font-mono h-[15dvh] flex items-end text-xs text-justify'>
+                <span>
+                    Ao continuar, você concorda com os Termos de Serviço e Política de Privacidade da [Sem nome] e em receber e-mails periódicos com atualizações.
+                </span>
+            </div>
         </section>
     )
 }
---
+++ src/components/ui/input.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-sm border border-[#2e2e2e] bg-[#212121] text-white outline-none px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#6e6e6e] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
