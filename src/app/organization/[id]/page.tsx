5c467c99863f12a1e71ff8cfd00096b44964d18a
Kleuber Silva <kleuber20@hotmail.com>
2024-01-29 11:46:41 -0300

    add page organization by id

---
+++ src/app/organization/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function Organization({ params }: { params: { id: string } }) {
+    return (
+        <div>
+            {params.id}
+            organzation
+        </div>
+    )
+}
--- src/components/organization/Organization.tsx
+++ src/components/organization/Organization.tsx
@@ -9,6 +9,7 @@ import useInfoUser from "@/hooks/use-info-user"
 import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Organization() {
 
@@ -47,22 +48,25 @@ export default function Organization() {
                             <CreateOrganization />
                         </CommandItem>
                         {organizationsData?.map((organization: any) => (
-                            <CommandItem
-                                key={organization.id}
-                                value={organization.name_organization.toUppercase}
-                                onSelect={(currentValue) => {
-                                    setValue(currentValue === value ? "" : currentValue);
-                                    setOpen(false);
-                                }}
-                            >
-                                {organization.name_organization}
-                                <CheckIcon
-                                    className={cn(
-                                        "ml-auto h-4 w-4 text-secondary",
-                                        value === organization.name_organization ? "opacity-100" : "opacity-0"
-                                    )}
-                                />
-                            </CommandItem>
+                            <Link href={`/organization/${organization.id}`} className="cursor-pointer">
+                                <CommandItem
+                                    className="cursor-pointer"
+                                    key={organization.id}
+                                    value={organization.name_organization.toUppercase}
+                                    onSelect={(currentValue) => {
+                                        setValue(currentValue === value ? "" : currentValue);
+                                        setOpen(false);
+                                    }}
+                                >
+                                    {organization.name_organization}
+                                    <CheckIcon
+                                        className={cn(
+                                            "ml-auto h-4 w-4 text-secondary",
+                                            value === organization.name_organization ? "opacity-100" : "opacity-0"
+                                        )}
+                                    />
+                                </CommandItem>
+                            </Link>
                         ))}
                     </CommandGroup>
                 </Command>
