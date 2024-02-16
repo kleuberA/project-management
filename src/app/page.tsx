21c2385017433a47d7d0b24ab3d5cba3500288d1
Kleuber Silva <kleuber20@hotmail.com>
2024-02-16 19:19:50 -0300

    Add button home if user is authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import ContainerIndex from "@/components/index/Index";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Index | Project Management App",
+  description: "Page of index aplication.",
+};
 
 export default function Home() {
   return (
--- src/components/index/Index.tsx
+++ src/components/index/Index.tsx
@@ -3,9 +3,23 @@ import { TextGenerateEffect } from "../text-generate-effect";
 import { Spotlight } from "../spot-light";
 import { Button } from "../ui/button";
 import { motion } from "framer-motion";
+import useSupabase from "@/hooks/useSupabase";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function ContainerIndex() {
     const words = "Your platform for quick and efficient project management."
+    const supabase = useSupabase();
+    const [session, setSession] = useState<boolean>(false);
+
+    useEffect(() => {
+        async function getSession() {
+            let { data } = await supabase.auth.getSession();
+            if (data?.session?.user) setSession(true);
+            else setSession(false);
+        }
+        getSession();
+    })
 
     return (
         <div
@@ -15,8 +29,23 @@ export default function ContainerIndex() {
                 animate={{ opacity: 1, translateX: 1 }}
                 transition={{ duration: 1 }}
                 className="absolute top-5 right-5 z-20 flex flex-row gap-3">
-                <Button variant="primary" className="">SignIn</Button>
-                <Button variant="secondary" >SignUp</Button>
+                {!session && (
+                    <div>
+                        <Link href="/auth/signin">
+                            <Button variant="primary" className="">SignIn</Button>
+                        </Link>
+                        <Link href="auth/signup">
+                            <Button variant="secondary" >SignUp</Button>
+                        </Link>
+                    </div>
+                )}
+                {session && (
+                    <div>
+                        <Link href="/home">
+                            <Button variant="primary" className="">Home</Button>
+                        </Link>
+                    </div>
+                )}
             </motion.div>
             <Spotlight
                 className="-top-40 left-0 md:left-60 md:-top-20"
